9a593a45f093b80cce2e8b997eead051e51426d0
jodylslaughter <jody.l.slaughter@gmail.com>
2026-02-12 11:55:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -96,7 +96,6 @@ class BaseCombatScene extends Phaser.Scene {
       .setDepth(20);
 
     this.physics.add.collider(this.player, this.enemies, this.onPlayerHitEnemy, undefined, this);
-    this.add.collider(this.player, this.enemies, this.onPlayerHitEnemy, undefined, this);
     this.physics.add.overlap(this.playerAttack, this.enemies, this.onAttackHitEnemy, undefined, this);
 
     this.lastAttackAt = 0;
